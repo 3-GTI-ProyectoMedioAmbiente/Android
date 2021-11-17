1b82ec9f92a1d523fbac4e9aa3e1c0874f921308
JuanC99 <juancarloshr123@gmail.com>
2021-11-17 16:54:38 +0100

    Auto stash before merge of "develop" and "origin/develop"

---
+++ src/AppBeacon/app/src/main/java/com/example/jcherram/appbeacon/VincularDispositivoFragment.java
@@ -0,0 +1,49 @@
+package com.example.jcherram.appbeacon;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class VincularDispositivoFragment extends Fragment {
+
+    public VincularDispositivoFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment BeaconsFragment.
+     */
+    public static VincularDispositivoFragment newInstance(String param1, String param2) {
+        VincularDispositivoFragment fragment = new VincularDispositivoFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            // TODO: Rename and change types of parameters
+            //mParam1 = getArguments().getString(ARG_PARAM1);
+            //String mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_vincular_nodo,
+                container, false);
+
+
+        return view;
+    }
+}
