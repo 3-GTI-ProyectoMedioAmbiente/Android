a10e91bd832de953772082d12e4f7750d5d78429
Sergi <sergisise@gmail.com>
2021-12-14 11:57:24 +0100

    Modelo usuario corregido

--- src/AppBeacon/app/src/main/java/com/example/jcherram/appbeacon/modelo/Usuario.java
+++ src/AppBeacon/app/src/main/java/com/example/jcherram/appbeacon/modelo/Usuario.java
@@ -1,10 +1,18 @@
 package com.example.jcherram.appbeacon.modelo;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.util.Locale;
+
 /**
+ *
  * Usuario.java
  * @author Sergi Sirvent Sempere
  * Fecha: 14/12/21
  * Clase que contiene el modelo del objeto usuario
+ *
  */
 
 public class Usuario {
@@ -20,7 +28,22 @@ public class Usuario {
     private String telefono;
     private String password;
 
-    //constructor para un autobusero
+
+    /**
+     * Constructor para un autobusero, el cual tiene en cuenta todos los atributos de la clase
+     *
+     * @param id
+     * @param mail
+     * @param nombre
+     * @param apellidos
+     * @param isAutobusero
+     * @param edad
+     * @param matricula
+     * @param telefono
+     * @param password
+     *
+     * id:Z,mail:Texto,nombre:Texto,apellidos:Texto,isAutobusero:VoF,edad:Z,matricula:Texto,telefono:Texto -> constructor()
+     */
     public Usuario(int id, String mail, String nombre, String apellidos, Boolean isAutobusero, int edad, String matricula, String telefono, String password) {
         this.id = id;
         this.mail = mail;
@@ -33,7 +56,19 @@ public class Usuario {
         this.password = password;
     }
 
-    //constructor para no autobusero
+    /**
+     * Constructor para no autobusero, que no tiene como parámetro los atributos isAutobusero y de matricula
+     *
+     * @param id
+     * @param mail
+     * @param nombre
+     * @param apellidos
+     * @param edad
+     * @param telefono
+     * @param password
+     *
+     * id:Z,mail:Texto,nombre:Texto,apellidos:Texto,edad:Z,telefono:Texto -> constructor()
+     */
     public Usuario(int id, String mail, String nombre, String apellidos, int edad, String telefono, String password) {
         this.id = id;
         this.mail = mail;
@@ -48,13 +83,20 @@ public class Usuario {
 
     }
 
-    //getters y setters
+    /**
+     *
+     * Getters y setters
+     *
+     */
+
 
     //id
     public int getId() {
         return id;
     }
-
+    public void setId(int id) {
+        this.id = id;
+    }
     //mail
     public String getMail() {
         return mail;
@@ -125,5 +167,30 @@ public class Usuario {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    /**
+     * Método que transforma al usuario en un objeto json
+     *
+     * @return json - Objeto json con los datos del usuario
+     * @throws JSONException
+     *
+     * toJson() -> jsonObject
+     */
+
+    public JSONObject toJson() throws JSONException {
+        JSONObject json = new JSONObject();
+
+        json.put("id_usuario", id);
+        json.put("mail",mail);
+        json.put("nombre", nombre);
+        json.put("apellidos",apellidos);
+        json.put("isAutobusero",isAutobusero);
+        json.put("edad",edad);
+        json.put("matricula",matricula);
+        json.put("telefono",telefono);
+        json.put("password",password);
+
+        return  json;
     }
 }
