742434573fa2226f84f0151f2571d712a50d5978
Alberto <albertitovalls97@gmail.com>
2021-12-15 20:19:56 +0100

    cambios registro y login

--- src/AppBeacon/app/src/main/java/com/example/jcherram/appbeacon/RegisterActivity.java
+++ src/AppBeacon/app/src/main/java/com/example/jcherram/appbeacon/RegisterActivity.java
@@ -3,6 +3,9 @@ package com.example.jcherram.appbeacon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
 
 
 // -----------------------------------------------------------------------------------
@@ -13,6 +16,9 @@ import android.os.Bundle;
 // -----------------------------------------------------------------------------------
 public class RegisterActivity extends AppCompatActivity {
 
+    Switch aSwitch;
+    EditText editText;
+
     /**
      *
      * @param savedInstanceState
@@ -21,5 +27,22 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        aSwitch=(Switch) findViewById(R.id.switch1);
+        editText=(EditText) findViewById(R.id.matriculaText);
+    }
+
+
+    public void onclick(View view){
+    if(view.getId()==R.id.switch1){
+        if(aSwitch.isChecked()){
+
+            editText.setVisibility(View.VISIBLE);
+        }else{
+            editText.setVisibility(View.INVISIBLE);
+
+        }
+        }
+
     }
 }
